1a8d5b9aabecfe4240f4a3185b057b966d97175e
Yash Jain <yash.jain@stonybrook.edu>
2018-05-13 22:38:06 -0400

    Removed while loop

--- gpsd.py
+++ gpsd.py
@@ -4,26 +4,24 @@ import gps
 session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
 
-while True:
-    try:
-        report = session.next()
-            # Wait for a 'TPV' report and display the current time
-            # To see all report data, uncomment the line below
-        #print (report + "\n")
-        if report['class'] == 'TPV':
-            if hasattr(report, 'lat'):
-                print ("Latitude: ", report.lat, " Longitude: ", report.lon)
-            #sys.exit(0)
-        else:
-            print('No fix found.')
-            
-        break
 
-    except KeyError:
-            pass
-    except KeyboardInterrupt:
-            quit()
-    except StopIteration:
-            session = None
-            print ("GPSD has terminated")
+try:
+    report = session.next()
+        # Wait for a 'TPV' report and display the current time
+        # To see all report data, uncomment the line below
+    #print (report + "\n")
+    if report['class'] == 'TPV':
+        if hasattr(report, 'lat'):
+            print ("Latitude: ", report.lat, " Longitude: ", report.lon)
+        #sys.exit(0)
+    else:
+        print('No fix found.')
+
+except KeyError:
+        pass
+except KeyboardInterrupt:
+        quit()
+except StopIteration:
+        session = None
+        print ("GPSD has terminated")
   
